2b46709fe819ddeaec2c88a8a98251ebe870a2c3
ksson <mrsonks@gmail.com>
2020-01-13 15:44:40 +0900

    처음 git 소스 업로드

--- app/src/main/java/com/aos/seobyapp/MainActivity.java
+++ app/src/main/java/com/aos/seobyapp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -11,10 +12,16 @@ import android.widget.Toast;
 
 import com.aos.seobyapp.FirebaseInterface;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.dynamiclinks.FirebaseDynamicLinks;
+import com.google.firebase.dynamiclinks.PendingDynamicLinkData;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.InstanceIdResult;
 import com.google.firebase.messaging.FirebaseMessaging;
+
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,7 +38,48 @@ public class MainActivity extends AppCompatActivity {
 
         // 화면뷰 트래킹
         ieFirebase.SendFirebaseEvent("PAGEVIEW", "APP_메인", "MainActivity", null);
-        Toast.makeText(getApplicationContext(), "앱 화면뷰 페이지뷰를 전송하였습니다",Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), "앱 화면뷰 페이지뷰를 전송하였습니다", Toast.LENGTH_LONG).show();
+
+        // DeepLink 처리 (Manual)
+        Uri data = this.getIntent().getData();
+        if (data != null && data.isHierarchical()) {
+            String uri = this.getIntent().getDataString();
+            Log.i("seobyapp", "Deep link clicked " + uri);
+        }
+
+        // 동적링크 처리 (Firebase Dynamic Link)
+
+        FirebaseDynamicLinks.getInstance()
+                .getDynamicLink(getIntent())
+                .addOnSuccessListener(this, new OnSuccessListener<PendingDynamicLinkData>() {
+                    @Override
+                    public void onSuccess(PendingDynamicLinkData pendingDynamicLinkData) {
+                        Uri deepLink = null;
+                        if (pendingDynamicLinkData != null) {
+                            deepLink = pendingDynamicLinkData.getLink();
+
+                            String viewName = deepLink.getQueryParameter("view");
+
+                            if (viewName.equals("Detail")) {
+
+                                // 앱 클릭 이벤트 트래킹
+                                ieFirebase.SendFirebaseEvent("EVENT", "APP_메인", "클릭", "Detail뷰호출");
+                                Toast.makeText(getApplicationContext(), "앱 Detail뷰호출 클릭 이벤트를 전송하였습니다",Toast.LENGTH_LONG).show();
+
+                                Intent intent = new Intent(getApplicationContext(), DetailActivity.class);
+                                startActivity(intent);
+                            }
+
+                        }
+                    }
+                })
+                .addOnFailureListener(this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "getDynamicLink:onFailure", e);
+                    }
+                });
+
     }
 
     public void btnEvent_Click(View v)
@@ -50,5 +98,16 @@ public class MainActivity extends AppCompatActivity {
 
         Intent intent = new Intent(getApplicationContext(), ViewWebview.class);
         startActivity(intent);
+    }
+
+    public void btnDetail_Click(View v)
+    {
+        // 앱 클릭 이벤트 트래킹
+        ieFirebase.SendFirebaseEvent("EVENT", "APP_메인", "클릭", "Detail뷰호출");
+        Toast.makeText(getApplicationContext(), "앱 Detail뷰호출 클릭 이벤트를 전송하였습니다",Toast.LENGTH_LONG).show();
+
+        Intent intent = new Intent(getApplicationContext(), DetailActivity.class);
+        startActivity(intent);
+
     }
 }
